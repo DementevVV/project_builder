c065b6d1de5530c1257f38fa5921df1ecbd8a365
DementevVV <dem_dvv@mail.ru>
2019-08-24 13:29:38 +0300

    better

--- main.rb
+++ main.rb
@@ -55,7 +55,102 @@ class AppBuilder
     system "cd #{data_hash['app_path']}/#{data_hash['app_name']} && rails db:migrate"
   end
 
+  def angular_js_material(data_hash)
+    if data_hash.key?('angular_js_material')
+      if data_hash['angular_js_material']
+        path = "#{data_hash['app_path']}/#{data_hash['app_name']}"
+        begin
+          puts "Creating folder for angular"
+          puts `cd #{path} && mkdir ./app/assets/javascripts/app-0000-angular`
+        rescue Exception => e
+          puts "Folder exists"
+        end
+        
+        puts "Downloading angular JS files"
+        `wget -c https://ajax.googleapis.com/ajax/libs/angularjs/1.7.6/angular.min.js                         -O  #{path}/app/assets/javascripts/app-0000-angular/app-0000-0-angular.js  -q --show-progress`
+        `wget -c https://ajax.googleapis.com/ajax/libs/angularjs/1.7.6/angular-animate.min.js                 -O  #{path}/app/assets/javascripts/app-0000-angular/app-0000-1-animate.js  -q --show-progress`
+        `wget -c https://ajax.googleapis.com/ajax/libs/angularjs/1.7.6/angular-aria.min.js                    -O  #{path}/app/assets/javascripts/app-0000-angular/app-0000-2-aria.js     -q --show-progress`
+        `wget -c https://ajax.googleapis.com/ajax/libs/angularjs/1.7.6/angular-messages.min.js                -O  #{path}/app/assets/javascripts/app-0000-angular/app-0000-3-messages.js -q --show-progress`
+        `wget -c https://ajax.googleapis.com/ajax/libs/angular_material/1.1.12/angular-material.min.js        -O  #{path}/app/assets/javascripts/app-0000-angular/app-0000-4-material.js -q --show-progress`
+        `wget -c https://cdnjs.cloudflare.com/ajax/libs/angular-file-upload/2.5.0/angular-file-upload.min.js  -O  #{path}/app/assets/javascripts/app-0000-angular/app-0000-6-uploader.js -q --show-progress`
+
+        File.open("#{path}/app/assets/javascripts/app-0000-angular/app-0000-9999-main.js", "w") do |out|
+          out.puts "var app = angular.module('#{data_hash['app_name']}', ['ngMaterial','ngMessages', 'angularFileUpload']);"
+        end
+
+        puts "Downloading angular CSS files"
+        `wget -c https://ajax.googleapis.com/ajax/libs/angular_material/1.1.12/angular-material.min.css -O #{path}/app/assets/stylesheets/angular_material.min.css -q --show-progress`
+        File.open("#{path}/app/views/layouts/application.html.erb") do |i|
+          File.open("output", 'w') do |o|
+            while line = i.gets
+              o.puts line
+              if line.chomp == "<head>"
+                o.puts "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\" />"
+              end
+            end
+          end
+        end
+        `mv output #{path}/app/views/layouts/application.html.erb`
+
+        content = File.open("#{path}/app/views/layouts/application.html.erb", "r").read
+        if !content.include?("ng-app")
+          content["<html"] = "<html ng-app='#{data_hash['app_name']}' ng-cloak"
+          File.open("#{path}/app/views/layouts/application.html.erb", "w") do |out|
+            out.puts content
+          end 
+        end
+      end
+    end
+  end
+
+  def turbolinks(data_hash)
+    if data_hash.key?('turbolinks')
+      if data_hash['turbolinks'] == false
+        #  REMOVE TURBOLINKS
+        `sed -i.bak "s/gem 'turbolinks'/#gem 'turbolinks'/g" #{data_hash['app_path']}/#{data_hash['app_name']}/Gemfile`
+        `sed -i.bak 's/reload/false/' #{data_hash['app_path']}/#{data_hash['app_name']}/app/views/layouts/application.html.erb`
+        `sed -i.bak 's@//= require turbolinks@@g' #{data_hash['app_path']}/#{data_hash['app_name']}/app/assets/javascripts/application.js`
+      end
+    end
+  end
+
   def gems(data_hash)
+    if data_hash.key?('gems')
+      File.open("#{data_hash['app_path']}/#{data_hash['app_name']}/Gemfile", 'a'){ |file| file.puts data_hash['gems'] }
+      system "cd #{data_hash['app_path']}/#{data_hash['app_name']} && bundle install"
+    end
+  end
+
+  def devise(data_hash)
+    if data_hash.key?('devise')
+      if data_hash['devise']
+        system "cd #{data_hash['app_path']}/#{data_hash['app_name']} && rails generate devise:install"
+        File.open("#{data_hash['app_path']}/#{data_hash['app_name']}/config/environments/development.rb") do |i|
+          File.open("output", 'w') do |o|
+            while line = i.gets
+              o.puts line
+              if line.chomp == "Rails.application.configure do"
+                o.puts "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }"
+              end
+            end
+          end
+        end
+        `mv output #{data_hash['app_path']}/#{data_hash['app_name']}/config/environments/development.rb`
+      end
+      system "cd #{data_hash['app_path']}/#{data_hash['app_name']} && rails generate devise User && rails db:migrate"
+      system "cd #{data_hash['app_path']}/#{data_hash['app_name']} && rails generate devise:views"
+    end
+  end
+
+  def fa(data_hash)
+    if data_hash.key?('devise')
+      if data_hash['devise']
+        puts "Adding font-awesome"
+        system "mkdir #{data_hash['app_path']}/#{data_hash['app_name']}/app/assets/webfonts"
+        system "cp font-awesome/* #{data_hash['app_path']}/#{data_hash['app_name']}/app/assets/webfonts/"
+        system "mv #{data_hash['app_path']}/#{data_hash['app_name']}/app/assets/webfonts/app-000-fa.css #{data_hash['app_path']}/#{data_hash['app_name']}/app/assets/stylesheets/"
+      end
+    end
   end
 end
 
@@ -73,3 +168,18 @@ app.db(data_hash)
 
 ## app skeleton
 app.skeleton(data_hash)
+
+## add angular js material
+app.angular_js_material(data_hash)
+
+## turbolinks
+app.turbolinks(data_hash)
+
+## gems
+app.gems(data_hash)
+
+## devise install
+app.devise(data_hash)
+
+## add fa
+app.fa(data_hash)
